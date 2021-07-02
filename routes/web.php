0db814ec414678a04bf690686646b8ca84482552
LucasMouraMacedo <lucasdemouramacedo@gmail.com>
2021-07-02 15:16:39 -0300

    adicionando todos os assetse criando as paginas no site de maneira estatica

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::namespace('App\Http\Controllers\Site')->group(function(){
-    Route::get('/', 'HomeController'); // Rota para o Controller HomeController
+    Route::get('/', 'HomeController')->name('site.home'); // Rota para o Controller HomeController
 
-    Route::get('produtos', 'CategoryController@index'); // Rota para o método do CategoryController, reponsável por retornar a página de categorias/produtos
-    Route::get('produtos/{slug}', 'CategoryController@show'); // Rota para o médoto show do CategoryController, responsável por retornar a view que mostra os produtos da categoria de acordo com o slug
+    Route::get('produtos', 'CategoryController@index')->name('site.products'); // Rota para o método do CategoryController, reponsável por retornar a página de categorias/produtos
+    Route::get('produtos/{slug}', 'CategoryController@show')->name('site.products.category'); // Rota para o médoto show do CategoryController, responsável por retornar a view que mostra os produtos da categoria de acordo com o slug
 
-    Route::get('blog', 'BlogController'); // Rota para o BlogController
+    Route::get('blog', 'BlogController')->name('site.blog'); // Rota para o BlogController
 
-    Route::view('sobre', 'site.about.index'); // Rota direto para view Sobre, por ser uma página estática
+    Route::view('sobre', 'site.about.index')->name('site.about'); // Rota direto para view Sobre, por ser uma página estática
 
-    Route::get('contato', 'ContactController@index'); //Rota para o médoto index do  ContactController, responsável por retornar a view de Contato
-    Route::post('contato', 'ContactController@form'); //Rota para o método form do ContactController, responsável por criar a lógica do formulário de contato
+    Route::get('contato', 'ContactController@index')->name('site.contact'); //Rota para o médoto index do  ContactController, responsável por retornar a view de Contato
+    Route::post('contato', 'ContactController@form')->name('site.contact.form'); //Rota para o método form do ContactController, responsável por criar a lógica do formulário de contato
 });
